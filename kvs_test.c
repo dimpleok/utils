5ccb529e269fa53139bd3db78b70a4201dccf4aa
dimpleok <wanjiushi@126.com>
2025-05-10 19:44:48 +0800

    Remove unused code.

--- kvs_test.c
+++ kvs_test.c
@@ -1,13 +1,11 @@
+#include <stdio.h>
 #include "kvs.h"
-#include "stdio.h"
 
 int main()
 {
    KVStore *store = kvs_create();
-   //int value = 99999;
    kvs_put(store, "hello", "{\"name\": \"kvstore\"}");
    void *ret = kvs_get(store, "hello");
-   //printf("ret=%d\n", *((int*)ret));
    printf("%s\n", (char*)ret);
    kvs_destroy(store);
    return 0;
