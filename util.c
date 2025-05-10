8e41aecf22ab4588b2deac5b5489f4463917463d
dimpleok <wanjiushi@126.com>
2025-05-10 19:51:36 +0800

    Format code.

--- util.c
+++ util.c
@@ -9,11 +9,11 @@ MemChunk *g_chunk = NULL;
 static void initialChunk(MemChunk** chunk, size_t totalSize)
 {
     *chunk = (MemChunk*) malloc(sizeof(MemChunk));
-    if( *chunk == NULL )
+    if (*chunk == NULL )
         return;
 
     (*chunk)->mem = malloc(totalSize);
-    if( (*chunk)->mem ) {
+    if ((*chunk)->mem != NULL) {
         memset( (*chunk)->mem, '\x0', totalSize);
         (*chunk)->totalSize = (*chunk)->freeSize = totalSize;
     } else {
@@ -24,12 +24,12 @@ static void initialChunk(MemChunk** chunk, size_t totalSize)
 // This method should be called to reclaim memory
 void destroyChunk()
 {
-    MemChunk** chunk = &g_chunk;
+    MemChunk **chunk = &g_chunk;
 
-    if( *chunk == NULL)
+    if (*chunk == NULL)
         return;
-    if( (*chunk)->mem != NULL ) {
-        free( (*chunk)->mem);
+    if ((*chunk)->mem != NULL) {
+        free((*chunk)->mem);
         (*chunk)->mem = NULL;
         //printf("%d bytes reclaimed\n", (*chunk)->totalSize);
         //printf("%d bytes used\n", (*chunk)->totalSize - (*chunk)->freeSize);
@@ -38,24 +38,24 @@ void destroyChunk()
     }
 }
 
-static void* requestMem(MemChunk** chunk, size_t size)
+static void* requestMem(MemChunk **chunk, size_t size)
 {
     // Initial the chunk if required
-    if(*chunk == NULL) {
-        initialChunk( chunk, CHUNK_SIZE);
+    if (*chunk == NULL) {
+        initialChunk(chunk, CHUNK_SIZE);
         if( *chunk == NULL) {
            return NULL;
         }
     }
 
     // Only for small memory allocation
-    if( size > 1024 ) {
+    if (size > 1024) {
         fprintf(stderr, "Only support small memory (<1024 Bytes) allocation\n");
         return NULL;
     }
 
     // Acquire memory from chunk
-    if( size > (*chunk)->freeSize ) {
+    if (size > (*chunk)->freeSize) {
         return NULL;
     } else {
         size_t offset = (*chunk)->totalSize - (*chunk)->freeSize;
@@ -73,12 +73,12 @@ void* _malloc(size_t size)
 char* _strdup(const char* src)
 {
     int n = strlen(src);
-    char* p = (char*)_malloc(n+1);
+    char *p = (char*)_malloc(n+1);
     strncpy(p, src, n); 
     return p;
 }
 
-int addKeyValue(KeyVal **head, const char* key, const char* val) 
+int addKeyValue(KeyVal **head, const char *key, const char *val) 
 {
     // New node containing key and value
     KeyVal *kv = (KeyVal*)_malloc(sizeof(KeyVal));
@@ -87,7 +87,7 @@ int addKeyValue(KeyVal **head, const char* key, const char* val)
     kv->value = _strdup(val);
 
     // Put the node into list
-    if( *head == NULL ) {
+    if (*head == NULL) {
         *head = kv;
     } else {
         kv->next = *head;
@@ -99,8 +99,8 @@ int addKeyValue(KeyVal **head, const char* key, const char* val)
 int containsKey(KeyVal *head, const char* key)
 {
     // Iterate the list and find the key
-    while( head ) {
-        if( strcmp(head->key, key) == 0) {
+    while (head != NULL) {
+        if (strcmp(head->key, key) == 0) {
            return 1;
         } else {
            head = head->next;
@@ -113,8 +113,8 @@ char* getValue(KeyVal *head, const char* key)
 {
     // Iterate the list and find the key
     char *value = NULL;
-    while( head ) {
-        if( strcmp(head->key, key) == 0) {
+    while (head != NULL) {
+        if (strcmp(head->key, key) == 0) {
            value = head->value; 
            break;
         } else {
@@ -134,14 +134,14 @@ void getArguments(int argc, char* argv[], int keyc, const char* keyMap[], KeyVal
 
     key[1] = '\x0';
     memset(optstring, '\x0', sizeof(optstring));
-    for(i = 0; i < keyc; i++) {
+    for (i = 0; i < keyc; i++) {
         snprintf(&optstring[i*2], 3, "%c:", keyMap[i][0]);
         key[0] = keyMap[i][0];
         addKeyValue(&kv, key, keyMap[i] + 2);
     }
     while ((opt = getopt(argc, argv, optstring)) != -1) {
         key[0] = opt;
-        if( containsKey(kv, key) ) {
+        if (containsKey(kv, key)) {
             addKeyValue(keyValues, getValue(kv, key), optarg);
         }
     }
